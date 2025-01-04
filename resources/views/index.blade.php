f9c5522161947eef5764aacdd8ae4a324c39aa7f
Tarhini089 <160245024+Tarhini089@users.noreply.github.com>
2025-01-04 10:53:03 +0200

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<h1>
+hello i am next to hsen jaafar!
+</h1>
+
+
+<div>
+    @if (count($task))
+        <div>there are a tasks!</div>
+    @else
+        <div>there is no tasks!</div>
+    @endif
+
+</div>
