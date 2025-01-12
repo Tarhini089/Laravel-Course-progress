3f226723e41a29f4a08fa122165b9efd75c3069b
Tarhini089 <160245024+Tarhini089@users.noreply.github.com>
2025-01-12 17:09:56 +0200

    section 18

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use laravel\sanctum\hasapitokens;
+
 
 class User extends Authenticatable
 {
